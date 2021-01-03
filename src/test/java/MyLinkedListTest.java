857746baad4e89e1df4d8752143cf8554c564083
sanket2498 <sanketdulange24@gmail.com>
2021-01-03 22:48:38 +0530

    Added : Adding elements to the end of list

--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -13,6 +13,20 @@ public class MyLinkedListTest {
         boolean result = myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void given3NumbersWhenLinkedShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        boolean result = myLinkedList.head.equals(myFirstNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+
 }
 
 
