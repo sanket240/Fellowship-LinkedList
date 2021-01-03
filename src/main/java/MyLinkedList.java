a179d4a5d272d76567563e5531f9d03abc8fc223
sanket2498 <sanketdulange24@gmail.com>
2021-01-03 22:50:33 +0530

    Added : Adding elements to the end of list

--- src/main/java/MyLinkedList.java
+++ src/main/java/MyLinkedList.java
@@ -1,9 +1,55 @@
 public class MyLinkedList {
-    public INode head;
-    public INode tail;
+    public INode<K> head;
+    public INode<K> tail;
     public MyLinkedList()
     {
-        this.head=null;
-        this.tail=null;
+        this.head=head;
+        this.tail=tail;
+    }
+    public void add(INode<K> myNode)
+    {
+        if (this.tail==null)
+        {
+            this.tail=myNode;
+        }
+        if (this.head==null)
+        {
+            this.head=myNode;
+        }
+        else {
+            INode tempNode=this.head;
+            this.head=myNode;
+            this.head.setNext(tempNode);
+        }
+    }
+    public void printMyNodes()
+    {
+        StringBuffer myNodes = new StringBuffer("MyNodes");
+        INode tempNode=head;
+        while (tempNode.getNext()!=null)
+        {
+            myNodes.append(tempNode.getKey());
+            if (!(tempNode.equals(tail))) {
+                myNodes.append("->");
+            }
+            tempNode=tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+    public void append(INode myNode) {
+        if(this.head==null)
+        {
+            this.head=myNode;
+        }
+        if (this.tail==null)
+        {
+            this.tail=myNode;
+
+        }
+        else {
+            this.tail.setNext(myNode);
+            this.tail=myNode;
+        }
     }
 }
